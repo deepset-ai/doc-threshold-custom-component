aa01b1f2452d1496872f69e33bd728394874d514
robpasternak <pasternakrs@gmail.com>
2024-10-01 16:51:17 +0200

    Add thresholder

---
+++ src/dc_custom_component/thresholders/thresholder.py
@@ -0,0 +1,25 @@
+from typing import List, Dict
+from haystack import Document, component
+
+@component
+class Thresholder:
+    """
+    A component that takes a list of documents and returns only
+    those documents whose score is greater than a given threshold.
+    """
+
+    def __init__(
+        self,
+        threshold: float,
+    ):
+        self.threshold = threshold
+
+    @component.output_types(documents=List[Document])
+    def run(self, documents: List[Document]) -> Dict[str, List[Document]]:
+        """
+        Filter documents based on the score.
+
+        :param documents: List of documents to filter.
+        :returns: List of documents whose score is greater than the threshold.
+        """
+        return {"documents" : [doc for doc in documents if doc.score >= self.threshold]}
--- tests/test_thresholder.py
+++ tests/test_thresholder.py
@@ -1,13 +1,14 @@
+from typing import List
 from haystack import Document
 from dc_custom_component.thresholders.thresholder import Thresholder
 
 
 def test_thresholder_empty() -> None:
-    documents = []
+    documents: List[Document] = []
     thresholder = Thresholder(threshold=0.5)
     assert thresholder.run(documents)["documents"] == []
 
 def test_thresholder_nonempty() -> None:
-    documents = [Document(content="a", score = 0.6), Document(content="b", score = 0.4)]
+    documents: List[Document] = [Document(content="a", score = 0.6), Document(content="b", score = 0.4)]
     thresholder = Thresholder(threshold=0.5)
     assert thresholder.run(documents)["documents"] == [Document(content="a", score = 0.6)]
